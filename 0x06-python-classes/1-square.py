b5879c39ad6f347781ba470028e9f730e138215d
charlyhackr <930@holbertonschool.com>
2019-10-01 16:05:38 -0500

    add class square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+class Square:
+    """A square"""
+
+    def __init__(self, size):
+        """INitialize a new square.
+
+        Args:
+            size (int): THe size for the new square.
+        """
+        self.__size = size
