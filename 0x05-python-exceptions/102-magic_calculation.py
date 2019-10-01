2918b6c7021c0d3695150b79b559da8977b85eed
charlyhackr <930@holbertonschool.com>
2019-09-30 20:31:51 -0500

    fix name  variable i + result

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
     result = 0
+    
     for i in range(1, 3):
         try:
             if > a:
                 raise Exception("Too far")
             else:
-                result += a ** b / i
+                result += a ** b / i + result
         except:
             result = b + a
             break
