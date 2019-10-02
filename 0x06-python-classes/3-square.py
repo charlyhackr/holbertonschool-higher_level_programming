de370f6155afef117c3b8099792eea40c460011e
charlyhackr <930@holbertonschool.com>
2019-10-01 21:07:07 -0500

    add square area

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+class Square:
+    def __init__(self, size=0):
+        """INitialize a new square.
+        
+        Args:
+            size int: The size for the new square.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Return the current area the square."""
+        return (self.__size * self.__size)
