26f643fa30338b32b7139d86d13de0be1bc0cfd3
charlyhackr <930@holbertonschool.com>
2019-10-01 22:37:33 -0500

    fixed comments for  the methosd

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -4,7 +4,11 @@ import math
 
 
 class MagiClass:
+    """Represent a circle"""
+    
     def __init__(self, radius=0):
+        """INitialize a magiClass. """
+        
         self.__radius = 0
 
         if type(radius) is not int and type(radius) is not float:
@@ -13,10 +17,13 @@ class MagiClass:
 
     @property
     def radius(self):
+        """Return the radius of the magicclass"""
         return self.__radius
 
     def area(self):
+        """Return the area of magicclass"""
         return self.__radius ** 2 * math.pi
 
     def circumference(self):
+        """Return the circumference of the magicclass"""
         return 2 * math.pi * self.__radius
