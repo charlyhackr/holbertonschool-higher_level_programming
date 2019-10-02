93c98858579389e51b2f453e9e076991ae13e989
charlyhackr <930@holbertonschool.com>
2019-10-01 21:07:48 -0500

    add square with private atrribute

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+class Square:
+    def __init__(self, size=0):
+        """INitialize a new square
+
+        ARgs:
+            size int: The size of the new square.
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """Geterr and setterr the current size of the square."""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """ Return at current area of the square"""
+        return (self.__size * self.size)
