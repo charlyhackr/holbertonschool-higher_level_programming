b66a79496b6054cb82c256922a54d9ec5c4cd9af
charlyhackr <930@holbertonschool.com>
2019-10-05 09:42:11 -0500

    fix typeError 22 caracteres

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -3,6 +3,6 @@ def text_indentation(text):
     """Print cositas
     """
     if text is None or not isinstance(text, str) or len(text) < 0:
-        raise TypeError("text must be an string")
+        raise TypeError("text must be a string")
     linea = "".join([cd if cd not in "?.:" else cd + "\n\n" for cd in text])
     print("\n".join([lin_el.strip() for lin_el in linea.split("\n")]), end="")
