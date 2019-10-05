46f5d5a967c1ebc90ee7cd75c2281ac699fcc6fb
charlyhackr <930@holbertonschool.com>
2019-10-04 23:10:35 -0500

    add text identation

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def text_indentation(text):
+    """Print cositas
+    """
+    if text is None or not isinstance(text, str) or len(text) < 0:
+        raise TypeError("Text must be an string")
+    linea = "".join([cad if cad not in "?.:" else cad + "\n\n" for cad in text])
+    print("\n".join([lin_el.strip() for lin_el in linea.split("\n")]), end="")
