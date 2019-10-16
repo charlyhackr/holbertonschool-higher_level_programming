83d0ccf066838648001fad3dc1aa39f5ef489c8a
charlyhackr <930@holbertonschool.com>
2019-10-16 17:04:16 -0500

    add pascal triangle

---
+++ 0x0B-python-input_output/14-pascal_triangle.py
@@ -0,0 +1,18 @@
+# 14-pascal_triangle.py
+
+
+def pascal_triangle(n):
+    """Represent Pascal Triangle of size n"""
+
+    if n <= 0:
+        return []
+
+    triangles = [[1]]
+    while len(triangles) != n:
+        tria = triangles[-1]
+        temp = [1]
+        for m in range(len(tria) - 1):
+            temp.append(tria[m] + tria[m + 1])
+        temp.append(1)
+        triangles.append(temp)
+    return triangles
