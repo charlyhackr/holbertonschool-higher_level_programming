71f36e60c154035dc6a1839fa74442360e9cbfea
charlyhackr <930@holbertonschool.com>
2019-10-16 16:24:31 -0500

    define a student

---
+++ 0x0B-python-input_output/13-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+class Student:
+    """create student"""
+
+    def __init__(self, first_name, last_name, age):
+        """ initialize  new student"""
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ dictionary representation of student"""
+
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {m: getattr(self, m) for m in attrs if hasattr(self, m)}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """ Replace all attributes of students """
+
+        for m, al in json.items():
+            setattr(self, m, al)
