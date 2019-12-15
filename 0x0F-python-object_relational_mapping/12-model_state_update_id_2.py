c30ee70c45a32304d38c9cb941515d2afc84b8a9
charlyhackr <930@holbertonschool.com>
2019-12-15 16:25:45 -0500

    fix trauling whitespace

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# Changes the name of the State object with id=2 
+# Changes the name of the State object with id=2
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
