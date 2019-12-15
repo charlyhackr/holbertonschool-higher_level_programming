168435c2b0e3619a9ded2c99795d7cad066c2c36
charlyhackr <930@holbertonschool.com>
2019-12-14 22:38:27 -0500

    fix -

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -7,4 +7,4 @@ if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     op = db.cursor()
     op.execute("SELECT * FROM `states`")
-    [print(s-tate) for state in op.fetchall() if state[1] == sys.argv[4]]
+    [print(state) for state in op.fetchall() if state[1] == sys.argv[4]]
