a1f088c10cc96e7b235a0b367262cdf01eb0d660
charlyhackr <930@holbertonschool.com>
2020-01-06 17:18:08 -0500

    post fiz

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
-#!bin/bash
-#takes in a URL, sends a POST request to the passed URL, and displays the body
+#!/bin/bash
+# Bash scripts that sends a POST request to a given url
 curl -s -X POST -d "email=hr@holbertonschool.com&subject=I will always be here for PLD" "$1"
